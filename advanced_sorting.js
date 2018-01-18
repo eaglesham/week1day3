44c24ec3c51264da71afc1657875dd334b13cd53
Edward <edward.eaglesham@gmail.com>
2018-01-18 19:36:58 +0000

    first commit. sorting function working

---
+++ advanced_sorting.js
@@ -0,0 +1,21 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+function sortingFunction(studentsArr) {
+  var alpha = studentsArr.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1
+    } else if (a.name > b.name) {
+      return 1
+    } else {
+      return b.age - a.age;
+    }
+  });
+  console.log(alpha);
+}
+
+sortingFunction(students);
