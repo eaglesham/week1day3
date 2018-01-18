a2132028366afbaf059b29f7e0f35c3ab39ab4dd
Edward <edward.eaglesham@gmail.com>
2018-01-18 17:07:16 +0000

    modfied code to add print of index of where waldo was

--- findwaldo.js
+++ findwaldo.js
@@ -2,13 +2,14 @@
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
+function actionWhenFound(j) {
   console.log("Found him!");
+  console.log("Waldo found at index: " + j);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
