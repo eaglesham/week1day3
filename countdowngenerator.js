c4a9c9acfb19b34facbfc5bd828b7cb2066e30f7
Edward <edward.eaglesham@gmail.com>
2018-01-18 23:08:28 +0000

    count down generator working well.

---
+++ countdowngenerator.js
@@ -0,0 +1,25 @@
+var countdownGenerator = function (x) {
+  var announcement = function () {
+    if (x > 0) {
+    console.log('T-minus ' + x);
+    x--;
+    } else if (x == 0) {
+      console.log('Blast Off!');
+      x--;
+    } else {
+      console.log('Rockets already gone, bub!');
+      x--;
+    }
+  }
+
+  return announcement;
+};
+
+var countdown = countdownGenerator(3);
+
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
